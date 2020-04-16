c26825fa35b22e1411f09baad19a54f8ccfd06b9
Florin <florinxhgds@gmail.com>
2020-04-16 11:16:41 +0300

    course-4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,29 +15,20 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Stage window;
-    Scene scene1, scene2;
+    Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
+        window.setTitle("Nevastuica Rock");
 
-        Label label1 = new Label("Welcome to the first scene. ");
-        Button button1 = new Button("Go to scene 2.");
-        button1.setOnAction(e -> window.setScene(scene2));
+        button = new Button("Click Me");
 
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
 
-        Button button2 = new Button("Go to scene 1.");
-        button2.setOnAction(e -> window.setScene(scene1));
-
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 300);
-
-        window.setScene(scene1);
-        window.setTitle("pagini");
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
         window.show();
     }
 
