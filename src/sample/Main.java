84fb092f8b638304845d6aeb5aa3c327a811d6a0
Florin <florinxhgds@gmail.com>
2020-04-16 12:50:15 +0300

    Embedding Layouts

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,39 +8,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
 
 public class Main extends Application {
 
     Stage window;
-    Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
         window.setTitle("Nevastuica Rock");
 
-        window.setOnCloseRequest(e -> {
-            e.consume();
-            closeProgram();
-        });
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
 
-        button = new Button("Close Program");
-        button.setOnAction(e -> closeProgram());
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
 
-        Scene scene = new Scene(layout, 300, 250);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300, 250);
         window.setScene(scene);
         window.show();
-    }
-
-    private void closeProgram(){
-        Boolean answer = ConfirmBox.display("Title","Are you sure you want to exit?");
-        if(answer)
-            window.close();
     }
 
     public static void main(String[] args) {
