94d3d5e95f6a3f7fc9503574673bd4cadf987c88
Florin <florinxhgds@gmail.com>
2020-04-17 11:53:42 +0300

    ChoiceBox

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,14 +7,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javax.xml.soap.Text;
+import java.awt.*;
 
 public class Main extends Application {
 
@@ -27,32 +29,33 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("MEA");
 
-        //Checkboxes
-        CheckBox box1 = new CheckBox("Rock");
-        CheckBox box2 = new CheckBox("Jazz");
-        box2.setSelected(true);
-
         //Button
         button = new Button("Order Now");
-        button.setOnAction(e -> handleOptions(box1, box2));
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+
+        //getItems returns the ObservableList object which you can add item to
+        choiceBox.getItems().add("Apples");
+        choiceBox.getItems().add("Bananas");
+        choiceBox.getItems().addAll("Bacon", "Ham", "Meatballs");
+        choiceBox.setValue("Apples");
+
+        button.setOnAction( e -> getChoice(choiceBox));
 
         //Layout
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(box1, box2, button);
+        layout.getChildren().addAll(choiceBox, button);
+        layout.setStyle("-fx-background-color: #012345;");
 
         scene= new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-    private void handleOptions(CheckBox box1, CheckBox box2){
-        String message = "Users preferred genres: ";
-        if(box1.isSelected())
-            message += "rock ";
-        if(box2.isSelected())
-            message += "jazz ";
-        System.out.println(message);
+    private void getChoice(ChoiceBox<String> choiceBox){
+        String food = choiceBox.getValue();
+        System.out.println(food);
     }
 
     public static void main(String[] args) {
