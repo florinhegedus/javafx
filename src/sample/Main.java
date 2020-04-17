41bae70a705da1fe7d9f1db3f40775bce8771a5c
Florin <florinxhgds@gmail.com>
2020-04-17 11:32:04 +0300

    CheckBox

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -18,37 +19,40 @@ import javax.xml.soap.Text;
 public class Main extends Application {
 
     Stage window;
+    Scene scene;
     Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
-        window.setTitle("Nevastuica Rock");
+        window.setTitle("MEA");
 
-        //Form
-        TextField nameInput = new TextField();
-        button = new Button("Click Me");
-        button.setOnAction(e -> isInt(nameInput, nameInput.getText()));
+        //Checkboxes
+        CheckBox box1 = new CheckBox("Rock");
+        CheckBox box2 = new CheckBox("Jazz");
+        box2.setSelected(true);
+
+        //Button
+        button = new Button("Order Now");
+        button.setOnAction(e -> handleOptions(box1, box2));
 
         //Layout
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(nameInput, button);
+        layout.getChildren().addAll(box1, box2, button);
 
-        Scene scene= new Scene(layout, 300, 250);
+        scene= new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-    private boolean isInt(TextField input, String message){
-        try{
-            int age = Integer.parseInt(input.getText());
-            System.out.println("User is: " + age);
-            return true;
-        }catch(NumberFormatException e){
-            System.out.println("Error: " + message + "is not a number.");
-            return false;
-        }
+    private void handleOptions(CheckBox box1, CheckBox box2){
+        String message = "Users preferred genres: ";
+        if(box1.isSelected())
+            message += "rock ";
+        if(box2.isSelected())
+            message += "jazz ";
+        System.out.println(message);
     }
 
     public static void main(String[] args) {
